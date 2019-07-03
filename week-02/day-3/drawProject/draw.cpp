e544709903da969d017f3bbc5c884d8a9a5e1c71
Pap Levente <natural.born.engineering@gmail.com>
2019-07-03 11:28:19 +0200

    added middle lines

--- week-02/day-3/drawProject/draw.cpp
+++ week-02/day-3/drawProject/draw.cpp
@@ -1,17 +1,22 @@
 #include "draw.h"
 
+
+// draw a red horizontal line to the canvas' middle.
+// draw a green vertical line to the canvas' middle.
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 void draw(SDL_Renderer* gRenderer) {
     //choose color
     SDL_SetRenderDrawColor(gRenderer, 0xFF /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
 //draw line
-    SDL_RenderDrawLine(gRenderer, 0, 0, 200, 200);
+    SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT/2);
 
     //choose color
-    SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0x18);
-//create a rectangle
-    SDL_Rect fillRect = { 100, 100, 200, 200};
-//draw rectangle
-    SDL_RenderFillRect( gRenderer, &fillRect );
+    SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+    SDL_RenderDrawLine(gRenderer, SCREEN_WIDTH/2, 0, SCREEN_WIDTH/2, SCREEN_HEIGHT);
+
 
 }
 
--- week-02/day-3/drawProject/draw2.cpp
+++ week-02/day-3/drawProject/draw2.cpp
@@ -1,7 +1,42 @@
 #include "draw.h"
+// Draw a box that has different colored lines on each edge.
+// The center of the box should align with the center of the screen.
 
-void draw(SDL_Renderer* gRenderer) {
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int boxHeight = 231;
+const int boxWidth = 301;
+
+void draw(SDL_Renderer *gRenderer)
+{
+
+    //choose color
+    SDL_SetRenderDrawColor(gRenderer, 0xFF /*R*/, 0x00 /*G*/, 0x00 /*B*/, 0xFF /*A*/);
+//draw upper line
+    SDL_RenderDrawLine(gRenderer, (SCREEN_WIDTH - boxWidth) / 2, (SCREEN_HEIGHT - boxHeight) / 2,
+                       (SCREEN_WIDTH - boxWidth) / 2 + boxWidth, (SCREEN_HEIGHT - boxHeight) / 2);
+
+    //choose color
+    SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+    // draw lower line
+    SDL_RenderDrawLine(gRenderer, (SCREEN_WIDTH - boxWidth) / 2, (SCREEN_HEIGHT - boxHeight) / 2 + boxHeight,
+                       (SCREEN_WIDTH - boxWidth) / 2 + boxWidth, (SCREEN_HEIGHT - boxHeight) / 2 + boxHeight);
+
+    //choose color
+    SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
+    // draw left line
+    SDL_RenderDrawLine(gRenderer, (SCREEN_WIDTH - boxWidth) / 2, (SCREEN_HEIGHT - boxHeight) / 2,
+                       (SCREEN_WIDTH - boxWidth) / 2, (SCREEN_HEIGHT - boxHeight) / 2 + boxHeight);
+    //choose color
+    SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
+    // draw right line
+    SDL_RenderDrawLine(gRenderer, (SCREEN_WIDTH - boxWidth) / 2 + boxWidth, (SCREEN_HEIGHT - boxHeight) / 2,
+                       (SCREEN_WIDTH - boxWidth) / 2 + boxWidth, (SCREEN_HEIGHT - boxHeight) / 2 + boxHeight);
+
+// would be nicer loading up a matrix based on the box sizes, and than getting the x-y coordinates into the drawer functions from there
 }
+
+
 
 //
 // Created by Dell on 2019. 07. 03..
