3bae94462b3ecfaa6ac47177340db40281e0ad66
Pap Levente <natural.born.engineering@gmail.com>
2019-07-17 20:36:13 +0200

    Make mileToKm more accurate

--- week-01/day-4/mileToKm/main.cpp
+++ week-01/day-4/mileToKm/main.cpp
@@ -5,11 +5,11 @@ int main() {
 
     double distanceInMiles;
 
-    std::cout << "Hello, please enter the distance to convert in miles: ";
+    std::cout << "Hello, please enter the distance to convert from miles into kilometres: ";
     std::cin >> distanceInMiles;
     std::cout << std::endl << std::fixed << std::setprecision(3) << distanceInMiles << " miles equals ";
 
-    double distanceInKms = 1.6 * distanceInMiles;
+    double distanceInKms = 1.609 * distanceInMiles;
 
     std::cout << std::fixed  << std::setprecision(3) << distanceInKms << " kilometres.";
 
